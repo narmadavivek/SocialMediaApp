2fbcf473534f1ed96e29c47c9938492d870de819
narmadavivek <145418529+narmadavivek@users.noreply.github.com>
2025-03-26 10:36:45 +0530

    Update index.js

--- server/dbConfig/index.js
+++ server/dbConfig/index.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-const MONGODB_URL = "mongodb+srv://narmadaviveka:PhOFUQlZdlzyMRqX@cluster0.no2ip.mongodb.net/blogwave";
-
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URL);
